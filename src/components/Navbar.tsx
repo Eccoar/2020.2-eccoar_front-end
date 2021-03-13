8548e7caf3cd92d3a4908366b506a447855316ce
Brian Pina <brian.pina2001@gmail.com>
2021-03-13 15:19:09 -0300

    Add responsiveness to the isotype

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,17 +4,17 @@ import isotipo from '../assets/isotipo.svg';
 import '../styles/navbar.scss';
 
 const Navbar: React.FC = () => {
-    return (
-        <nav className="navbar">
-            <div/>
-            <img src={isotipo} className="navbar__isotipo" />
-            <div className="navbar__hamburguer">
-                <div/>
-                <div/>
-                <div/>
-            </div>
-        </nav>
-    )
-}
+	return (
+		<nav className='navbar'>
+			<div />
+			<img src={isotipo} className='navbar__isotipo' />
+			<div className='navbar__hamburguer'>
+				<div />
+				<div />
+				<div />
+			</div>
+		</nav>
+	);
+};
 
 export default Navbar;
