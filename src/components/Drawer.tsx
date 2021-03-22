1c3725b55adee303d7b84c5ed5e490b11b250fed
MatheusBlanco <msallesblanco@gmail.com>
2021-03-22 16:31:39 -0300

    Fixing test and link in drawer

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -20,44 +20,44 @@ const Drawer: React.FC<DrawerProps> = ({ show, close }: DrawerProps) => {
 		<>
 			<nav className={drawerClass}>
 				<div className='side-drawer__content'>
-					<div>
-						<img
-							role='button'
-							className='side-drawer__close'
-							src={closeButton}
-							onClick={close}
-						/>
-						<div className='side-drawer__links'>
-							<br></br>
-							<Link
-								className='side-drawer__text'
-								to='/submit-complaint/infos'
-								onClick={close}
-							>
-								CRIAR DENÚNCIA
-							</Link>
-							<br></br>
-							<Link
-								className='side-drawer__text'
-								to='/'
-								onClick={close}
-							>
-								VER DENÚNCIAS
-							</Link>
-							<br></br>
-						</div>
+					<div className='side-drawer__button' onClick={close}>
+						<img className='side-drawer__close' src={closeButton} />
 					</div>
-          {colorChanger?.color === 'light' ? (
-            <div onClick={() => colorChanger?.changeTheme("dark")} className='side-drawer__mode'>
-              <img src={moon} className='side-drawer__toggle' />
-              <p className='side-drawer__bottom_text'>MODO ESCURO</p>
-            </div>
-          ) : (
-            <div onClick={() => colorChanger?.changeTheme("light")} className='side-drawer__mode'>
-              <img src={sun} className='side-drawer__toggle' />
-              <p className='side-drawer__bottom_text'>MODO CLARO</p>
-            </div>
-          )}
+					<div className='side-drawer__links'>
+						<br></br>
+						<Link
+							className='side-drawer__text'
+							to='/submit-complaint/infos'
+						>
+							CRIAR DENÚNCIA
+						</Link>
+						<br></br>
+						<Link className='side-drawer__text' to='/'>
+							VER DENÚNCIAS
+						</Link>
+						<br></br>
+					</div>
+					{colorChanger?.color === 'light' ? (
+						<div
+							onClick={() => colorChanger?.changeTheme('dark')}
+							className='side-drawer__mode'
+						>
+							<img src={moon} className='side-drawer__toggle' />
+							<p className='side-drawer__bottom_text'>
+								MODO ESCURO
+							</p>
+						</div>
+					) : (
+						<div
+							onClick={() => colorChanger?.changeTheme('light')}
+							className='side-drawer__mode'
+						>
+							<img src={sun} className='side-drawer__toggle' />
+							<p className='side-drawer__bottom_text'>
+								MODO CLARO
+							</p>
+						</div>
+					)}
 				</div>
 			</nav>
 		</>
