dfaae16db64bebe440c28edc0f9be490e11dd32a
phe0 <pheofo@gmail.com>
2021-03-23 19:21:01 -0300

    exclude test files from coverage

--- sonar-project.js
+++ sonar-project.js
@@ -7,11 +7,13 @@ sonarqubeScanner({
 	serverUrl: 'https://sonarcloud.io',
 	token: token,
 	options: {
-		'sonar.sources': './src',
-		'sonar.exclusions': './src/tests/**',
-		'sonar.tests': './src/tests',
+		'sonar.sources': 'src',
+		'sonar.exclusions': 'src/tests/**',
+		'sonar.tests': 'src/tests',
 		'sonar.test.inclusions':
-			'./src/tests/**/*.test.tsx,./src/tests/**/*.test.ts',
+			'src/tests/**/*.test.tsx,./src/tests/**/*.test.ts',
+		'sonar.coverage.exclusions':
+			'src/tests/components/**/*,src/tests/components/**/*',
 		'sonar.typescript.lcov.reportPaths': 'coverage/lcov.info',
 		'sonar.testExecutionReportPaths': 'reports/test-report.xml',
 		'sonar.organization': 'eccoar',
