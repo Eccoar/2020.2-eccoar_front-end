6b41907f7fb4333a659f9535ca316107ddbbc41e
phe0 <pheofo@gmail.com>
2021-03-23 15:19:49 -0300

    config sonarcloud

---
+++ sonar-project.js
@@ -0,0 +1,20 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+require('dotenv').config();
+const token = process.env.REACT_APP_SONAR_TOKEN;
+
+sonarqubeScanner({
+	serverUrl: 'https://sonarcloud.io',
+	token: token,
+	options: {
+		'sonar.sources': './src',
+		'sonar.exclusions': './src/tests/**',
+		'sonar.tests': './src/tests',
+		'sonar.test.inclusions':
+			'./src/tests/**/*.test.tsx,./src/tests/**/*.test.ts',
+		'sonar.typescript.lcov.reportPaths': 'coverage/lcov.info',
+		'sonar.testExecutionReportPaths': 'reports/test-report.xml',
+		'sonar.organization': 'eccoar',
+		'sonar.projectKey': 'Eccoar_2020.2-eccoar_front-end',
+	},
+});
