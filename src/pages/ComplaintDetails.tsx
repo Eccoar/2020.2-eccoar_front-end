c34d2b6eb84f9ef713bdf52347640712f6e8fb90
Victor-Buendia <victorbuendia03@gmail.com>
2021-03-31 14:31:39 -0300

    Fill component data into details page

--- src/pages/ComplaintDetails.tsx
+++ src/pages/ComplaintDetails.tsx
@@ -1,13 +1,22 @@
 import { useState } from 'react';
+import Button from '../components/Button';
 
-const ComplaintDetails = (props: any) => {
-	const [complaint] = useState(props.location.state);
+export interface ComplaintProps {
+	location: any; //Resolver o tipo correto
+}
+
+const ComplaintDetails = ({ location }: ComplaintProps) => {
+	const [complaint] = useState(location.state);
 
 	return (
 		<div>
-			<h1>{complaint.name}</h1>
-			<img src={complaint.picture} alt={`Foto de ${complaint.name}`} />
-			<p>{complaint.description}</p>
+			<h1>{complaint.complaint_name}</h1>
+			<img
+				src={complaint.complaint_picture}
+				alt={`Foto de ${complaint.complaint_name}`}
+			/>
+			<p>{complaint.complaint_description}</p>
+			<Button text='CONFIRMAR DENÚNCIA' />
 		</div>
 	);
 };
