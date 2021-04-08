3ae164b9959a64cd6618f78eb7cc27032b625466
Guilherme <marquesguilherme3@gmail.com>
2021-04-08 03:36:20 -0300

    Fix lint ci, add gateway env variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: `http://localhost:5000/api` });
+const url = process.env.REACT_APP_GATEWAY || 'localhost:5000';
+
+const api = axios.create({ baseURL: `http://${url}/api` });
 
 export default api;
