cc7b8ba30aaa631f9e11437b5dea773361352cf3
Guilherme <marquesguilherme3@gmail.com>
2021-04-08 04:17:35 -0300

    Use https on baseUrl

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const url = process.env.REACT_APP_GATEWAY || 'localhost:5000';
 
-const api = axios.create({ baseURL: `http://${url}/api` });
+const api = axios.create({ baseURL: `https://${url}/api` });
 
 export default api;
