8ad221194cbd2abda1a8405a904b60a56c36440d
Sabanai104 <gabrielsabanaitrindade@gmail.com>
2021-04-09 01:44:32 -0300

    create the complaint photo

--- src/pages/SubmitComplain/SubmitComplaitPhoto.tsx
+++ src/pages/SubmitComplain/SubmitComplaitPhoto.tsx
@@ -1,17 +1,43 @@
+import { ChangeEvent, useState, useMemo } from 'react';
 import { FiCamera } from 'react-icons/fi';
 import { useHistory } from 'react-router-dom';
 import Button from '../../components/Button';
 
 const SubmitComplaitPhoto = () => {
 	const history = useHistory();
+	const [photo, setPhoto] = useState<File | undefined>();
 	const onSubmit = () => {
 		history.push('/submit-complaint/location', history.location.state);
+	};
+
+	const onChangePhoto = (event: ChangeEvent) => {
+		const target = event.target as HTMLInputElement;
+		const file: File = (target.files as FileList)[0];
+		setPhoto(file);
 	};
 
 	return (
 		<div className='submitComplaint' data-testid='SubmitComplaintPhoto'>
 			<div />
-			<FiCamera />
+			{!photo ? (
+				<div>
+					<label className='submitComplaint__sendImageContainer'>
+						<FiCamera />
+						<input
+							type='file'
+							onChange={(event) => onChangePhoto(event)}
+							className='submitComplaint__sendImage'
+							accept='image/*'
+						/>
+					</label>
+				</div>
+			) : (
+				<div>{photo.name}</div>
+			)}
+			{/* <label className='submitComplaint__sendImageContainer'>
+				<FiCamera />
+				<input type="file" onChange={(event) => onChangePhoto(event)} className='submitComplaint__sendImage' />
+			</label> */}
 			<Button text='Continuar' onClick={onSubmit} />
 		</div>
 	);
