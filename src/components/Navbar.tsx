0e29251f97147923973599800b305c9080541487
Sabanai104 <gabrielsabanaitrindade@gmail.com>
2021-04-09 02:51:22 -0300

    #145 add the arrow button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import isotipo from '../assets/isotipo.svg';
+import arrow from '../assets/arrow.svg';
 import Drawer from './Drawer';
 import BackDrop from './Backdrop';
-import { Link } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
 	const [drawerOpen, setDrawerOpen] = useState(false);
+	const history = useHistory();
+
+	const { pathname } = useLocation();
 
 	const handleCloseDrawer = () => {
 		setDrawerOpen(false);
@@ -16,9 +20,17 @@ const Navbar: React.FC = () => {
 
 	return (
 		<nav className='navbar'>
+			{pathname == '/' ? (
+				<img src={arrow} className='navbar__noArrow' />
+			) : (
+				<img
+					onClick={() => history.goBack()}
+					src={arrow}
+					className='navbar__Arrow'
+				/>
+			)}
 			<Drawer close={handleCloseDrawer} show={drawerOpen} />
 			{backDrop}
-			<div />
 			<Link to='/'>
 				<img src={isotipo} className='navbar__isotipo' />
 			</Link>
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,5 @@
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
-
 import Home from './pages/Home';
 import SubmitComplaintInfos from './pages/SubmitComplain/SubmitComplaintInfos';
 import SubmitComplaitOptionPhoto from './pages/SubmitComplain/SubmitComplaitOptionPhoto';
@@ -8,7 +7,6 @@ import SubmitComplaintPhoto from './pages/SubmitComplain/SubmitComplaitPhoto';
 import SubmitComplaintGeolocation from './pages/SubmitComplain/SubmitComplaintGeolocation';
 import SubmitComplaintConclude from './pages/SubmitComplain/SubmitComplaintConclude';
 import ComplaintDetails from './pages/ComplaintDetails';
-
 const Routes = () => {
 	return (
 		<Router>
